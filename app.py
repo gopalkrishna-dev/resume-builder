d919d53e06345466dbf56c936e9ad5d648780482
Gopal Krishna <iamkrishna330@gmail.com>
2025-06-19 13:18:19 +0700

    fix: render rollback due to port binding

--- app.py
+++ app.py
@@ -31,5 +31,7 @@ def generate():
     )
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    import os
+    port = int(os.environ.get('PORT', 10000))  # Render dynamically gives this
     app.run(host='0.0.0.0', port=port)
+
